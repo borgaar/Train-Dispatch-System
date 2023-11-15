07a974530e0759fb0119f5081329860250484c80
Borgar <borgarbarland@gmail.com>
2023-11-15 02:03:14 +0100

    Added JavaDocs

--- src/main/java/edu/ntnu/stud/utils/Search.java
+++ src/main/java/edu/ntnu/stud/utils/Search.java
@@ -4,8 +4,18 @@ import edu.ntnu.stud.models.DepartureTable;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
+/**
+ * Class for searching the departure table.
+ */
 public class Search {
-  public static OptionalInt GetIndexByTrainId(DepartureTable table, int trainId) {
+  /**
+   * Method for getting the index of a train by its ID.
+   *
+   * @param table   the departure table.
+   * @param trainId the train ID.
+   * @return the index of the train.
+   */
+  public static OptionalInt getIndexByTrainId(DepartureTable table, int trainId) {
     return IntStream.range(0, table.getDepartureList().size())
         .filter(i -> table.getDepartureList().get(i).getTrainId() == trainId)
         .findFirst();
