735a33aecf1435261226e10ec9f8de20bee6cc7e
Borgar <borgarbarland@gmail.com>
2023-11-15 05:26:54 +0100

    Made some methods testable

--- src/main/java/edu/ntnu/stud/commands/CreateDeparture.java
+++ src/main/java/edu/ntnu/stud/commands/CreateDeparture.java
@@ -54,19 +54,17 @@ public class CreateDeparture {
    *
    * @return the track of the departure.
    */
-  private OptionalInt getTrack() {
+  public OptionalInt getTrack() {
     String trackString = inputHandler.getInput(
         "Enter the track of the departure, "
             + "or leave blank to set no track",
         "[1-5]",
         REGEX_TRACK_FORMAT,
-        true);
+        true).trim();
 
-    if (trackString.isEmpty()) {
-      return OptionalInt.empty();
-    }
-
-    return OptionalInt.of(Integer.parseInt(trackString));
+    return trackString.isEmpty()
+        ? OptionalInt.empty()
+        : OptionalInt.of(Integer.parseInt(trackString));
   }
 
   /**
@@ -74,13 +72,13 @@ public class CreateDeparture {
    *
    * @return the destination of the departure.
    */
-  private String getDestination() throws NoInputException {
+  public String getDestination() throws NoInputException {
     String destination = inputHandler.getInput(
         "Enter the destination of the departure, "
             + "or leave blank to abort departure creation",
         "[Any name]",
         REGEX_DESTINATION_FORMAT,
-        true);
+        true).trim();
 
     if (destination.isEmpty()) {
       throw new NoInputException("Input was empty.");
@@ -89,13 +87,13 @@ public class CreateDeparture {
     return destination;
   }
 
-  private LocalTime getDelay() throws NoInputException {
+  public LocalTime getDelay() throws NoInputException {
     String timeString = inputHandler.getInput(
         "Enter the delay of the departure, "
             + "or leave blank to abort departure creation",
         "HH:MM",
         REGEX_24HR_FORMAT,
-        true);
+        true).trim();
 
     if (timeString.isEmpty()) {
       throw new NoInputException("No input was detected.");
@@ -104,13 +102,13 @@ public class CreateDeparture {
     return LocalTime.parse(timeString);
   }
 
-  private LocalTime getTime(DepartureTable table, LocalTime delay) throws NoInputException {
+  public LocalTime getTime(DepartureTable table, LocalTime delay) throws NoInputException {
     String timeString = inputHandler.getInput(
         "Enter the scheduled time of departure (departure time, without delay), "
             + "or leave blank to abort creation",
         "HH:MM",
         REGEX_24HR_FORMAT,
-        true);
+        true).trim();
 
     if (timeString.isEmpty()) {
       throw new NoInputException("Input was empty.");
@@ -147,7 +145,7 @@ public class CreateDeparture {
             + "or leave blank to abort departure creation",
         "[ABCD][0-9]",
         REGEX_LINE_FORMAT,
-        true);
+        true).trim();
 
     if (line.isEmpty()) {
       throw new NoInputException("Input was empty.");
@@ -168,7 +166,7 @@ public class CreateDeparture {
             + "or leave blank to autogenerate a unique ID",
         "[1000-9999]",
         REGEX_TRAINID_FORMAT,
-        true);
+        true).trim();
 
     if (trainIdString.isEmpty()) {
       Random rnd = new Random();
---
+++ src/test/java/edu/ntnu/stud/input/InputHandlerTest.java
@@ -0,0 +1,15 @@
+package edu.ntnu.stud.input;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InputHandlerTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void getInput() {
+  }
+}
