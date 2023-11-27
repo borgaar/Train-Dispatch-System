d84bbecad09a5808a7f9d3083f683d174a05ad59
Borgar <borgarbarland@gmail.com>
2023-11-27 15:38:41 +0100

    Improved testing and added commenting

--- src/test/java/edu/ntnu/stud/input/InputHandlerTest.java
+++ src/test/java/edu/ntnu/stud/input/InputHandlerTest.java
@@ -30,83 +30,82 @@ public class InputHandlerTest {
   }
 
   // Helper method for converting an array of inputs to a single string simulating user input
-  private String toSingleInputString(String[] inputs) {
-    StringBuilder sb = new StringBuilder();
-    for (String input : inputs) {
-      sb.append(input).append("\n");
+  private void toInputStream(String[] userInputs) {
+    StringBuilder sb = new StringBuilder(); // Create a string builder
+
+    // Append each input to the string builder, separated by '\r\n' being the user pressing enter
+    for (String input : userInputs) {
+      sb.append(input).append("\r\n"); // '\r\n' simulates the user pressing enter
     }
-    return sb.toString();
+
+    String input = sb.toString(); // Convert the string builder to a string
+
+    // Set the input stream to the string
+    InputStream in = new ByteArrayInputStream(input.getBytes());
+    System.setIn(in);
   }
 
   // Test if the input handler returns the correct input when given valid user input
   @Test
   void testValidInput() {
-    String input = "a\n";
+    String[] input = {"a"}; // The user input to be simulated
 
-    InputStream in = new ByteArrayInputStream(input.getBytes());
-    System.setIn(in);
+    toInputStream(input); // Set the input stream to the list of inputs
 
-    InputHandler inputHandler = new InputHandler();
+    InputHandler inputHandler = new InputHandler(); // Create an input handler
 
-    String expected = "a";
-    String result = inputHandler.getInput("prompt", "format", "[a-z]", false);
+    String expected = "a";  // The expected result is 'a'
+    String result = inputHandler.getInput("prompt", "format", "[a-z]", false);  // The format is [a-z], so 'a' is valid
 
-    assertEquals(expected, result);
+    assertEquals(expected, result); // Check if the result is as expected
   }
 
   // Test if the input handler returns the correct input when given invalid user input first, then valid user input
   @Test
   void testInvalidInput() {
-    String[] listOfInputs = {"3", "a"};
+    String[] userInput = {"3", "a"};  // Simulates the user typing '3', then 'a'
 
-    String input = toSingleInputString(listOfInputs);
+    toInputStream(userInput); // Set the input stream to the list of inputs
 
-    InputStream in = new ByteArrayInputStream(input.getBytes());
-    System.setIn(in);
+    InputHandler inputHandler = new InputHandler(); // Create an input handler
 
-    InputHandler inputHandler = new InputHandler();
+    String expected = "a";  // The expected result is 'a'
+    String result = inputHandler.getInput("prompt", "format", "[a-z]", false);  // The format is [a-z], so '3' is invalid
 
-    String expected = "a";
-    String result = inputHandler.getInput("prompt", "format", "[a-z]", false);
-
-    assertEquals(expected, result);
-    assertTrue(outContent.toString().contains(INVALID_INPUT_MESSAGE));
+    assertEquals(expected, result); // Check if the result is as expected
+    assertTrue(outContent.toString().contains(INVALID_INPUT_MESSAGE));  // Should print invalid input message
   }
 
   // Test if the input handler returns the correct input when given blank user input first (when it's not allowed), then valid user input
   @Test
   void testBlankInputNotAllowed() {
-    String[] listOfInputs = {"", "a"};
+    String[] listOfInputs = {"", "a"};  // Simulates the user pressing enter without typing anything (blank input),
+                                        // followed by the user typing 'a'
+    toInputStream(listOfInputs);   // Set the input stream to the list of inputs
 
-    String input = toSingleInputString(listOfInputs);
+    InputHandler inputHandler = new InputHandler(); // Create an input handler
 
-    InputStream in = new ByteArrayInputStream(input.getBytes());
-    System.setIn(in);
+    String expected = "a";  // The expected result is 'a'
+    String result = inputHandler.getInput("prompt", "format", "[a-z]", false);  // Blank input is not allowed
 
-    InputHandler inputHandler = new InputHandler();
-
-    String expected = "a";
-    String result = inputHandler.getInput("prompt", "format", "[a-z]", false);
-
-    assertEquals(expected, result);
-    assertTrue(outContent.toString().contains(INVALID_INPUT_MESSAGE));
+    assertEquals(expected, result); // Check if the result is as expected
+    assertTrue(outContent.toString().contains(INVALID_INPUT_MESSAGE));  // Should print invalid input message
   }
 
   @Test
   void testBlankInputAllowed() {
-    String[] listOfInputs = {"", "a"};
+    String[] listOfInputs = {"", "a"};  // Simulates the user pressing enter without typing anything (blank input),
+                                        // followed by the user typing 'a'. Since blank input is allowed, the
+                                        // invalid input message should not be printed as the first input is valid.
 
-    String input = toSingleInputString(listOfInputs);
+    toInputStream(listOfInputs);     // Set the input stream to the list of inputs
 
-    InputStream in = new ByteArrayInputStream(input.getBytes());
-    System.setIn(in);
+    InputHandler inputHandler = new InputHandler(); // Create an input handler
 
-    InputHandler inputHandler = new InputHandler();
+    String expected = ""; // The expected result is blank input
+    String result = inputHandler.getInput("prompt", "format", "[a-z]", true); // Blank input is allowed
 
-    String expected = "";
-    String result = inputHandler.getInput("prompt", "format", "[a-z]", true);
-
-    assertEquals(expected, result);
-    assertFalse(outContent.toString().contains(INVALID_INPUT_MESSAGE));
-  }
+    assertEquals(expected, result); // Check if the result is as expected
+    assertFalse(outContent.toString().contains(INVALID_INPUT_MESSAGE)); // Should not print invalid input message
+  }                                                                     // as blank input is allowed
 }
