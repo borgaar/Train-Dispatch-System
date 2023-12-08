e9877753cc86f2ba1aac4312526b9903ef328c82
Borgar <borgarbarland@gmail.com>
2023-12-08 12:53:32 +0100

    Rearranged field declaration

--- src/main/java/edu/ntnu/stud/commands/Command.java
+++ src/main/java/edu/ntnu/stud/commands/Command.java
@@ -7,8 +7,8 @@ import edu.ntnu.stud.models.DepartureTable;
  * Base class for commands that can be run on the departure table.
  */
 public abstract class Command {
-  private final String name;
   public final InputHandler inputHandler;
+  private final String name;
 
   public Command(String name) {
     this.name = name;
